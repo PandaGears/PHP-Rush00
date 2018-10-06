a1e0bae7040ac2f92be6b796c0848c4901bdaaed
tamara RADLOFF <tradlof@c2r4s2.wethinkcode.co.za>
2018-10-06 16:41:50 +0200

    first step

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+?>
+
+<html>
+<head>
+	<title>WeThinkWed</title>
+
+	<link rel="stylesheet" href="style/style.css" media="all"/>
+</head>
+<body>
+	<div class="main_wrapper">
+		<div>generic header</div>
+		<div>generic menu </div>
+		<div>generic sidebar</div>
+		<div>generic content</div>
+		<div>generic footer</div>
+	</div>
+</body>
+</html> 
